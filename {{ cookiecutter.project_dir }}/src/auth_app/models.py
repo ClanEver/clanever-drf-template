ff669d65196f90c92847a3a69bd0b97e3d8cadbd
ClanEver <562211524@qq.com>
2024-01-14 23:30:53 +0800

    fix

--- {{ cookiecutter.project_dir }}/src/auth_app/models.py
+++ {{ cookiecutter.project_dir }}/src/auth_app/models.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+custom_username_validator = validators.RegexValidator(
+    r'^[a-zA-Z0-9]{5,100}$',
+    "5-100位，字母或数字"
+)
 
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text="5-100位，字母或数字",
+        validators=[custom_username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
