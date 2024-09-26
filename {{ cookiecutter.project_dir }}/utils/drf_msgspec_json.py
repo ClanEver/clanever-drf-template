78e9a29ce2a19d79a7c316b80c4df5ac3a835c74
ClanEver <562211524@qq.com>
2024-09-27 00:40:46 +0800

    feat: update

--- {{ cookiecutter.project_dir }}/utils/drf_msgspec_json.py
+++ {{ cookiecutter.project_dir }}/utils/drf_msgspec_json.py
@@ -1,7 +1,9 @@
+import datetime
 import uuid
 from decimal import Decimal
 from typing import Any
 
+import arrow
 import msgspec
 from django.conf import settings
 from django.utils.functional import Promise
@@ -10,16 +12,13 @@ from rest_framework.parsers import BaseParser
 from rest_framework.renderers import BaseRenderer
 from rest_framework.settings import api_settings
 
-__all__ = ['MsgspecJSONParser', 'MsgspecJSONRenderer', ]
+__all__ = ['MsgspecJSONParser', 'MsgspecJSONRenderer']
 
 
 class MsgspecJSONParser(BaseParser):
     media_type = 'application/json'
 
-    def parse(self, stream, media_type=None, parser_context=None):
-        """
-        Parses the incoming bytestream as JSON and returns the resulting data.
-        """
+    def parse(self, stream, media_type=None, parser_context=None):  # noqa
         parser_context = parser_context or {}
         encoding = parser_context.get('encoding', settings.DEFAULT_CHARSET)
 
@@ -27,25 +26,33 @@ class MsgspecJSONParser(BaseParser):
             data = stream.read().decode(encoding)
             return msgspec.json.decode(data)
         except msgspec.DecodeError as exc:
-            raise ParseError(f'JSON parse error - {exc}')
+            raise ParseError(f'JSON parse error - {exc}') from exc
 
 
-def enc_hook(obj: Any) -> Any:
+class JsonRenderError(TypeError):
+    pass
+
+
+def enc_hook(obj: Any) -> Any:  # noqa: PLR0911
+    if isinstance(obj, str | uuid.UUID | Promise):
+        return str(obj)
     if isinstance(obj, dict):
         return dict(obj)
-    elif isinstance(obj, list):
+    if isinstance(obj, list):
         return list(obj)
-    elif isinstance(obj, Decimal):
-        if api_settings.COERCE_DECIMAL_TO_STRING:
-            return str(obj)
-        else:
-            return float(obj)
-    elif isinstance(obj, (str, uuid.UUID, Promise)):
-        return str(obj)
-    elif hasattr(obj, "tolist"):
+    if isinstance(obj, datetime.datetime):
+        return obj.strftime('%Y-%m-%d %H:%M:%S')
+    if isinstance(obj, datetime.date):
+        return obj.strftime('%Y-%m-%d')
+    if isinstance(obj, arrow.Arrow):
+        return obj.format('YYYY-MM-DD HH:mm:ss')
+    if isinstance(obj, Decimal):
+        return str(obj) if api_settings.COERCE_DECIMAL_TO_STRING else float(obj)
+    if hasattr(obj, 'tolist'):
         return obj.tolist()
-    elif hasattr(obj, "__iter__"):
-        return list(item for item in obj)
+    if hasattr(obj, '__iter__'):
+        return list(obj)
+    raise JsonRenderError(f'Unknown type: {type(obj)}')
 
 
 class MsgspecJSONRenderer(BaseRenderer):
@@ -54,9 +61,9 @@ class MsgspecJSONRenderer(BaseRenderer):
     ensure_ascii = True
     charset = None
 
-    def render(self, data: Any, *args, **kwargs):
+    def render(self, data: Any, *args, **kwargs):  # noqa: ARG002
         if data is None:
-            return bytes()
+            return b''
 
         encoder = msgspec.json.Encoder(enc_hook=enc_hook)
         return encoder.encode(data)
