093a51c451dc6d8c094f0af24b14af27b704b065
ClanEver <TRjdiam@hotmail.com>
2024-10-14 10:41:00 +0800

    fix: celery cant USE_TZ=False

--- {{ cookiecutter.project_dir }}/{{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_dir }}/{{ cookiecutter.project_slug }}/settings.py
@@ -201,7 +201,7 @@ LANGUAGE_CODE = 'zh-hans'
 TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/zh-hans/5.1/howto/static-files/
