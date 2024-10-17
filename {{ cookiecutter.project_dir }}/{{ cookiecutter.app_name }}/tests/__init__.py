232d212026617a59617b81fbef56d563524f9e9d
ClanEver <TRjdiam@hotmail.com>
2024-10-17 10:36:02 +0800

    fix: app.tests

--- {{ cookiecutter.project_dir }}/{{ cookiecutter.app_name }}/tests/__init__.py
+++ {{ cookiecutter.project_dir }}/{{ cookiecutter.app_name }}/tests/__init__.py
@@ -1,1 +1,1 @@
-from {{ cookiecutter.app_name }}.tests.{{ cookiecutter.model_name_snake }} import NewModelTests, NewModelViewSetTests
+from {{ cookiecutter.app_name }}.tests.{{ cookiecutter.model_name_snake }} import {{ cookiecutter.model_name }}Tests, {{ cookiecutter.model_name }}ViewSetTests
