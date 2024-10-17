95062df10e0a80da3dd221a01e5aaf7463cb88f6
ClanEver <TRjdiam@hotmail.com>
2024-10-17 10:53:35 +0800

    style: debug

--- {{ cookiecutter.project_dir }}/debug/templatetags/combine_url.py
+++ {{ cookiecutter.project_dir }}/debug/templatetags/combine_url.py
@@ -3,6 +3,7 @@ from django.urls import URLResolver
 
 register = template.Library()
 
+
 @register.filter
 def combine_url(url_resolvers: list[URLResolver]):
     return ' '.join([str(url_resolver.pattern) for url_resolver in url_resolvers])
