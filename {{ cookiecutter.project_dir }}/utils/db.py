a336f50ce3b511796575d6584e4d9aecc5cd888c
ClanEver <TRjdiam@hotmail.com>
2024-11-04 18:19:03 +0800

    feat: BaseModel.__repr__

--- {{ cookiecutter.project_dir }}/utils/db.py
+++ {{ cookiecutter.project_dir }}/utils/db.py
@@ -4,13 +4,21 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
-    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+    create_time = models.DateTimeField(verbose_name=_('创建时间'), auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name=_('更新时间'), auto_now=True)
 
     class Meta:
         abstract = True
         verbose_name = 'BaseModel'
         verbose_name_plural = verbose_name
+
+    __repr_exclude__ = {'_state', 'create_time', 'update_time'}
+
+    def __repr__(self):
+        return (
+            f'<{self.__class__.__name__} '
+            f'{" ".join(f"{k}={v}" for k, v in self.__dict__.items() if k not in self.__repr_exclude__)}>'
+        )
 
 
 class NoBulkQuerySet(models.QuerySet):
